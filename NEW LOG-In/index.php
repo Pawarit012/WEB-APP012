21bbcf973d43a95df3cd5ecefacae27c503936ee
Pawarit012 <pawarit04336@gmail.com>
2025-10-09 20:02:50 +0700

    i

---
+++ NEW LOG-In/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register System</title>
+    <link rel="stylesheet" href="/NEW LOG-In/style.css">
+    
+</head>
+<body>
+    
+    <h1> hello php</h1>
+
+</body>
+</html>
