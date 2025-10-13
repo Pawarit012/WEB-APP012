55d0a6c317d4f372881c45531d7ead007b95248c
Pawarit012 <pawarit04336@gmail.com>
2025-10-13 19:49:16 +0700

    d

---
+++ testlogin/server.php
@@ -0,0 +1,17 @@
+<?php
+
+    $servername = 'localhost';
+    $username ='root';
+    $password = '';  
+    $dbname ='WEBAPP012';
+
+    //create connection
+    $conn = mysqli_connect($servername,$username,$password,$dbname);
+
+    if (!$conn) {
+        die('connection failed' . mysqli_connect_error());
+
+    } 
+
+
+?>
