9c96d9e303d6b81d4b3cf4e676c6e4b8c42ae732
Pawarit012 <pawarit04336@gmail.com>
2025-10-14 19:40:02 +0700

    ด

---
+++ LOGIN/login.php
@@ -0,0 +1,32 @@
+<?php
+    
+    include("server.php")
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="head"><h2>Welcome</h2></div>
+        
+        <form action="" method="POST" >
+            <label for="loginemail">email</label>
+            <input type="email" name="EMAIL" placeholder="กรอกemail">  <br>
+            <label for="password">Password</label>
+            <input type="password" name="PASSWORD" placeholder="กรอกรหัสผ่าน">
+            
+            
+        </form>
+        
+    </div>
+
+</body>
+</html>
